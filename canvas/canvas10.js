548e3afb139d974cc31cc5ba291d30e72fb5df40
shaoxi <shaoxi2093@foxmail.com>
2019-05-19 22:35:33 +0800

    feat: update radialGradient test

--- canvas/canvas10.js
+++ canvas/canvas10.js
@@ -2,18 +2,40 @@
 var canvas = document.getElementById('my-canvas')
 var ctx = canvas.getContext('2d')
 
-var radiusGradient = ctx.createRadialGradient(0, 100, 100, 0, 100, 0)
-radiusGradient.addColorStop(0, 'rgb(100, 0 ,0 )')
-radiusGradient.addColorStop(0.6, 'rgb(200, 0, 0')
-radiusGradient.addColorStop(1, 'rgb(255, 0, 0)')
+// var rg1 = ctx.createRadialGradient(40, 60, 30, 100, 120, 90)
+// rg1.addColorStop(0, 'rgb(255, 0, 0)')
+// rg1.addColorStop(0.9, 'rgb(255, 100, 0)')
+// rg1.addColorStop(1, 'rgba(255, 200, 0, 0')
 
-ctx.fillStyle = radiusGradient
-ctx.fillRect(0, 0, 200, 200)
+// ctx.fillStyle = rg1
+// ctx.fillRect(0,0, 300, 300)
 
+var radgrad = ctx.createRadialGradient(45,45,10,52,50,30);
+  radgrad.addColorStop(0, '#A7D30C');
+  radgrad.addColorStop(0.9, '#019F62');
+  radgrad.addColorStop(1, 'rgba(1,159,98,0)');
+  
+  var radgrad2 = ctx.createRadialGradient(105,105,20,112,120,50);
+  radgrad2.addColorStop(0, '#FF5F98');
+  radgrad2.addColorStop(0.75, '#FF0188');
+  radgrad2.addColorStop(1, 'rgba(255,1,136,0)');
 
-var radiusGradient2 = ctx.createRadialGradient(300, 80, 80, 300, 80, 0)
-radiusGradient2.addColorStop(0, 'white')
-radiusGradient2.addColorStop(1, 'red')
+  var radgrad3 = ctx.createRadialGradient(95,15,15,102,20,40);
+  radgrad3.addColorStop(0, '#00C9FF');
+  radgrad3.addColorStop(0.8, '#00B5E2');
+  radgrad3.addColorStop(1, 'rgba(0,201,255,0)');
 
-ctx.fillStyle = radiusGradient2
-ctx.fillRect(220, 0, 200, 160)
+  var radgrad4 = ctx.createRadialGradient(0,150,50,0,140,90);
+  radgrad4.addColorStop(0, '#F4F201');
+  radgrad4.addColorStop(0.8, '#E4C700');
+  radgrad4.addColorStop(1, 'rgba(228,199,0,0)');
+  
+  // 画图形
+  ctx.fillStyle = radgrad4;
+  ctx.fillRect(0,0,150,150);
+  ctx.fillStyle = radgrad3;
+  ctx.fillRect(0,0,150,150);
+  ctx.fillStyle = radgrad2;
+  ctx.fillRect(0,0,150,150);
+  ctx.fillStyle = radgrad;
+  ctx.fillRect(0,0,150,150);
