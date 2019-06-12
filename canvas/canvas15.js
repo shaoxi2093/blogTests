31afd11960b2d255c0990ae9ae04d98fad409e28
shaoxi <shaoxi2093@foxmail.com>
2019-06-12 16:06:08 +0800

    feat: add 1. 修改角度计算绘制刻度

--- canvas/canvas15.js
+++ canvas/canvas15.js
@@ -12,20 +12,33 @@ ctx.arc(100, 100, r, -0.5 * Math.PI, Math.PI * 1.5)
 ctx.stroke()
 
 ctx.translate(100, 100)
-// todo 画刻度 和 数字(坐标计算有错)
+// 画刻度 和 数字
+const degUnit = 2 * Math.PI / 360
 for(var i = 1; i < 61; i++) {
-  var len = i % 5 === 0 ? 12 : 4 // 刻度线长度
-  // 圆上的点A
-  var xA = Math.cos(360 * (i / 60)) * r
-  var yA = Math.sin(360 * (i / 60)) * r
-  // 圆里的点B
-  var xB = xA - Math.cos(360 * (i / 60)) * len
-  var yB = yA - Math.sin(360 * (i / 60)) * len
-
-  if(i % 5 == 0) {
-    console.log(xA,yA,xB,yB)
+  var len = i % 5 === 0 ? 10 : 4 // 刻度线长度
+  var xA,yA,xB,yB
+  radius = 180 * 1/30
+  if(i <= 30) {
+    // 圆上的点A
+    xA = Math.sin(Math.PI * i / 30) * r
+    yA = Math.cos(Math.PI * i / 30) * r
+    
+  } else {
+    xA = -Math.sin(360 * ((i - 30) / 60) * degUnit) * r
+    yA = -Math.cos(360 * ((i - 30) / 60) * degUnit) * r
   }
+  
+  // 圆里的点B
+  xB = xA - Math.sin(Math.PI * i / 30) * len
+  yB = yA - Math.cos(Math.PI * i / 30) * len
+
+  
   ctx.moveTo(xA, yA)
   ctx.lineTo(xB, yB)
   ctx.stroke()
+  if(i % 5 === 0) {
+    var xNum = xA - Math.sin(Math.PI * i / 30) * 14 - 4
+    var yNum = yA - Math.cos(Math.PI * i / 30) * 14 + 4
+    ctx.fillText(i / 5, xNum, yNum)
+  }
 }
